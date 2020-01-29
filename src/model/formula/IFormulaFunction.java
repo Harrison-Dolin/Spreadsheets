cd663441305cbff89969e75efa9f9c0c05798c4a
Harrison-Dolin <57839063+Harrison-Dolin@users.noreply.github.com>
2020-01-29 16:34:29 -0600

    Represents a function that is a formula {FormulaInterface}

--- src/model/formula/IFormulaFunction.java
+++ src/model/formula/IFormulaFunction.java
@@ -4,5 +4,5 @@ package edu.cs3500.spreadsheets.model.formula;
  * Represents a function that is a formula {FormulaInterface}.
  */
 public interface IFormulaFunction extends FormulaInterface {
-
+ 
 }
